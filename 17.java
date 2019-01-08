7714dab0a683ba1a576234230d74d6666b087a62
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2019-01-08 23:54:51 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A
+{
+public static void main(String[] arg)
+{
+Scanner scan = new Scanner(System.in);
+int n,k;
+n = scan.nextInt();
+k = scan.nextInt();
+System.out.println(n*k);
+}
+}
